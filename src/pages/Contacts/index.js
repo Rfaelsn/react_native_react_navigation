e9d45bbbbda67a5fe1240df16fba2f961fedb0ea
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-03 20:27:43 -0300

    feat: implementando passagem de dados entre pages

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -4,10 +4,36 @@ import {View,Text} from 'react-native';
 export default function Contacts({navigation}){
     return(
         <View style={{marginTop:40}}>
-            <Text>Rafael</Text>
-            <Text
-                onPress={() => navigation.navigate('Information')}
-            >Information...</Text>
+            <View>
+                <Text>Nome: Rafael Nascimento</Text>
+                <Text>Telefone: (91) 98763-4455</Text>
+                <Text
+                    onPress={() => navigation.navigate('Information',
+                    {
+                        nome: 'Rafael Silva do Nascimento',
+                        telefone: '(91) 98763-4455',
+                        endereco:'rua do fluna prox ao ciclano',
+                        numero:'710',
+                        profissao: 'Analista',
+                        email: 'rafael@email.com',
+                    })}
+                >Information...</Text>
+            </View>
+            <View style={{marginTop:40}}>
+                <Text>Nome: Joao Silva</Text>
+                <Text>Telefone: (91) 98733-4485</Text>
+                <Text
+                    onPress={() => navigation.navigate('Information',
+                    {
+                        nome: 'Joao da Silva Campos',
+                        telefone: '(91) 98763-3486',
+                        endereco:'rua do fluna prox ao ciclano',
+                        numero:'645',
+                        profissao: 'Analista',
+                        email: 'joao@email.com',
+                    })}
+                >Information...</Text>
+            </View>
         </View>
     );
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View,Text} from 'react-native';
 
-export default function Contacts(){
+export default function Contacts({ route }){
     return(
         <View style={{marginTop:40}}>
-            <Text>Information</Text>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>Endereco: {route.params?.endereco}</Text>
+            <Text>nº: {route.params?.numero}</Text>
+            <Text>;Profissao: {route.params?.profissao}</Text>
+            <Text>Email: {route.params?.email}</Text>
         </View>
     );
 }
