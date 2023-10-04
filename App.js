f584f3c4bdf0570933248a44d5fbd53433d87b35
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-03 22:55:00 -0300

    feat: implementando scrollView

--- App.js
+++ App.js
@@ -7,14 +7,60 @@ import Contacts from './src/pages/Contacts';
 import Information from './src/pages/Information';
 import AppContacts from "./src/pages/AppContacts";
 
+import {Ionicons} from '@expo/vector-icons'
+
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator();
 
 function Tabs(){
   return(
-    <Tab.Navigator>
-      <Tab.Screen name='AppContacts' component={AppContacts}/>
-      <Tab.Screen name='Contacts' component={Contacts}/>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#1BB57D',
+      }}
+    >
+      <Tab.Screen 
+        name='AppContacts'
+        component={AppContacts} 
+        options={{ 
+          tabBarIcon: ({color,size,focused}) => {
+            if(focused){
+              return <Ionicons 
+                name="home"
+                size={size}
+                color={color}
+              />
+            }
+
+            return <Ionicons 
+              name="home-outline" 
+              size={size}
+              color={color}
+            />
+          }
+        }}
+      />
+      <Tab.Screen 
+        name='Contacts'
+        component={Contacts}
+        options={{ 
+          tabBarIcon: ({color,size,focused}) => {
+            if(focused){
+              return <Ionicons 
+                name="person"
+                size={size}
+                color={color}
+              />
+            }
+
+            return <Ionicons 
+              name="person-outline" 
+              size={size}
+              color={color}
+            />
+          }
+        }}
+        />
     </Tab.Navigator>
   );
 }
@@ -23,7 +69,11 @@ export default function App(){
   return(
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='AppContacts' component={Tabs}/>
+        <Stack.Screen 
+          name='AppContacts' 
+          component={Tabs}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name='Information' component={Information}/>
       </Stack.Navigator>
     </NavigationContainer>
